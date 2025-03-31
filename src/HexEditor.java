e137651bb576fb68f4df82b570e2d3eedef4fd47
Julian <julianherbert.2007@gmail.com>
2025-03-31 13:24:33 -0400

    efwafwe

--- src/HexEditor.java
+++ src/HexEditor.java
@@ -7,9 +7,19 @@ class HexEditor extends JComponent {
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        int row = 0;
         int fontSize = 14;
         g.setFont(new Font("monospace", Font.PLAIN, fontSize));
-        
+        g.setColor(Color.BLACK);
+
+        for (int i = 0; i < bytes.length; i++) {
+            int row = i / 16;
+            int col = i - (row * 16);
+
+            if (row * fontSize < getHeight()) {
+                g.drawChars(String.format("%02X", bytes[i] & 0xff).toCharArray(), 0, 2, col * (fontSize * 2), row * fontSize);
+            } else {
+                break;
+            }
+        }
     }
 }
